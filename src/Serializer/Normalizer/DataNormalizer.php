4cc74d0ad39b4fb707592f76e2a23b98493a46e2
jprivet-dev <jean.philippe.rivet@gmail.com>
2023-01-11 18:42:36 +0100

    feat(DataNormalizer): adjust year traitment & force on null empty values

--- src/Serializer/Normalizer/DataNormalizer.php
+++ src/Serializer/Normalizer/DataNormalizer.php
@@ -3,12 +3,15 @@
 namespace App\Serializer\Normalizer;
 
 use App\Entity\Data;
+use App\Util\ArrayUtil;
 use Symfony\Component\Serializer\Exception\InvalidArgumentException;
+use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
 use Symfony\Component\Serializer\Normalizer\CacheableSupportsMethodInterface;
+use Symfony\Component\Serializer\Normalizer\DenormalizerInterface;
 use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
 use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
 
-class DataNormalizer implements NormalizerInterface, CacheableSupportsMethodInterface
+class DataNormalizer implements NormalizerInterface, DenormalizerInterface, CacheableSupportsMethodInterface
 {
     public function __construct(private ObjectNormalizer $normalizer)
     {
@@ -20,22 +23,39 @@ class DataNormalizer implements NormalizerInterface, CacheableSupportsMethodInte
             throw new InvalidArgumentException(sprintf('The object must implement "%s".', Data::class));
         }
 
-        $data = $this->normalizer->normalize($object, $format, $context);
+        // @see https://symfony.com/doc/current/components/serializer.html#using-callbacks-to-serialize-properties-with-object-instances
+        $callback = fn(
+            $innerObject,
+            $outerObject,
+            string $attributeName,
+            string $format = null,
+            array $context = []
+        ) => $innerObject instanceof \DateTime ? $innerObject->format('Y') : null;
 
-        if (isset($data['anneeDebut'])) {
-            $data['anneeDebut'] = $object->getAnneeDebut()->format('Y');
-        }
+        $context[AbstractNormalizer::CALLBACKS] = [
+            'anneeDebut' => $callback,
+            'anneeSeparation' => $callback,
+        ];
 
-        if (isset($data['anneeSeparation'])) {
-            $data['anneeSeparation'] = $object->getAnneeSeparation()->format('Y');
-        }
-
-        return $data;
+        return $this->normalizer->normalize($object, $format, $context);
     }
 
     public function supportsNormalization($data, string $format = null, array $context = []): bool
     {
         return $data instanceof Data;
+    }
+
+    public function denormalize(mixed $data, string $type, string $format = null, array $context = []): Data
+    {
+        $data = ArrayUtil::trim($data);
+        $data = ArrayUtil::emptyStringsAsNull($data);
+
+        return $this->normalizer->denormalize($data, Data::class);
+    }
+
+    public function supportsDenormalization(mixed $data, string $type, string $format = null): bool
+    {
+        return Data::class === $type;
     }
 
     public function hasCacheableSupportsMethod(): bool
