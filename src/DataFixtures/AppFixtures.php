d49d8f962485684956a13106f85e8963528fc8bb
jprivet-dev <jean.philippe.rivet@gmail.com>
2023-01-13 15:27:00 +0100

    feat(AppFixtures): create classic & admin users

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,15 +2,32 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
+    public function __construct(private UserPasswordHasherInterface $hasher)
+    {
+    }
+
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        // User classic
+        $user = new User();
+        $user->setEmail('user@email.com');
+        $user->setRoles(['ROLE_USER']);
+        $user->setPassword($this->hasher->hashPassword($user, 'password'));
+        $manager->persist($user);
+
+        // User admin
+        $admin = new User();
+        $admin->setEmail('admin@email.com');
+        $admin->setRoles(['ROLE_ADMIN']);
+        $admin->setPassword($this->hasher->hashPassword($admin, 'password'));
+        $manager->persist($admin);
 
         $manager->flush();
     }
