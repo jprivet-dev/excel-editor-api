4954d6a589c5e43d4a4a9c1ddd7a9ab57762e78b
jprivet-dev <jean.philippe.rivet@gmail.com>
2023-01-13 15:20:36 +0100

    build(composer): add orm-fixtures

--- config/bundles.php
+++ config/bundles.php
@@ -8,4 +8,5 @@ return [
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
 ];
