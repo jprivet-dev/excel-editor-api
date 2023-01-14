befe4fe8e1ea9525eec18d2d83774a97a8af0f06
jprivet-dev <jean.philippe.rivet@gmail.com>
2023-01-14 11:29:08 +0100

    feat(ExcelHeadersValidatorTest): create base of test

---
+++ tests/Validator/ExcelHeadersValidatorTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Tests\Validator;
+
+use PHPUnit\Framework\TestCase;
+
+class ExcelHeadersValidatorTest extends TestCase
+{
+    public function testSomething(): void
+    {
+        $this->assertTrue(true);
+    }
+}
