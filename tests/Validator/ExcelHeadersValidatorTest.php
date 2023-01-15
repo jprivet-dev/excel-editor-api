9f0cdf8d84e27ce72c1647dcaf81345bcb28f39c
jprivet-dev <jean.philippe.rivet@gmail.com>
2023-01-15 17:26:07 +0100

    feat(ExcelHeadersValidatorTest): complete base of test

--- tests/Validator/ExcelHeadersValidatorTest.php
+++ tests/Validator/ExcelHeadersValidatorTest.php
@@ -2,12 +2,51 @@
 
 namespace App\Tests\Validator;
 
-use PHPUnit\Framework\TestCase;
+use App\Validator\ExcelHeaders;
+use App\Validator\ExcelHeadersValidator;
+use Symfony\Component\Validator\Exception\UnexpectedValueException;
+use Symfony\Component\Validator\Test\ConstraintValidatorTestCase;
 
-class ExcelHeadersValidatorTest extends TestCase
+class ExcelHeadersValidatorTest extends ConstraintValidatorTestCase
 {
-    public function testSomething(): void
+    protected function createValidator()
     {
-        $this->assertTrue(true);
+        return new ExcelHeadersValidator();
+    }
+
+    public function testNullIsValid()
+    {
+        $this->validator->validate(null, new ExcelHeaders());
+        $this->assertNoViolation();
+    }
+
+    public function testEmptyIsValid()
+    {
+        $this->validator->validate('', new ExcelHeaders());
+        $this->assertNoViolation();
+    }
+
+    public function testThrowsExceptionIfNotStringCompatibleType()
+    {
+        $this->expectException(UnexpectedValueException::class);
+        $this->validator->validate(new \stdClass(), new ExcelHeaders());
+    }
+
+    /**
+     * @dataProvider provideInvalidValues
+     */
+    public function testInvalidValues($values)
+    {
+        $constrain = new ExcelHeaders();
+        $this->validator->validate($values, $constrain);
+
+        $this->buildViolation($constrain->message)
+            ->setParameter('{{ string }}', $values)
+            ->assertRaised();
+    }
+
+    public function provideInvalidValues(): iterable
+    {
+        yield ['%'];
     }
 }
