3270fa2e266d66fe90109613ef7f364b6d7b2aad
jprivet-dev <jean.philippe.rivet@gmail.com>
2023-01-15 17:25:46 +0100

    feat(ExcelHeadersValidator): complete base of validator

--- src/Validator/ExcelHeadersValidator.php
+++ src/Validator/ExcelHeadersValidator.php
@@ -4,20 +4,32 @@ namespace App\Validator;
 
 use Symfony\Component\Validator\Constraint;
 use Symfony\Component\Validator\ConstraintValidator;
+use Symfony\Component\Validator\Exception\UnexpectedTypeException;
+use Symfony\Component\Validator\Exception\UnexpectedValueException;
 
 class ExcelHeadersValidator extends ConstraintValidator
 {
     public function validate($value, Constraint $constraint)
     {
+        if (!$constraint instanceof ExcelHeaders) {
+            throw new UnexpectedTypeException($constraint, ExcelHeaders::class);
+        }
+
         /* @var ExcelHeaders $constraint */
 
         if (null === $value || '' === $value) {
             return;
         }
 
-        // TODO: implement the validation here
-        $this->context->buildViolation($constraint->message)
-            ->setParameter('{{ value }}', $value)
-            ->addViolation();
+        if (!\is_scalar($value) && !$value instanceof \Stringable) {
+            throw new UnexpectedValueException($value, 'string');
+        }
+
+        if (!preg_match('/^[a-zA-Z0-9]+$/', $value, $matches)) {
+            // the argument must be a string or an object implementing __toString()
+            $this->context->buildViolation($constraint->message)
+                ->setParameter('{{ string }}', $value)
+                ->addViolation();
+        }
     }
 }
